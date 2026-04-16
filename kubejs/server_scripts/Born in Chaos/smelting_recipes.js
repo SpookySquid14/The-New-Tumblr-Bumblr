8210ab65c1856edc9e01a725f6258a6804015050
SpookySquid14 <hunter.c.donald@gmail.com>
2026-04-16 14:51:43 -0400

    Tweaked some things, notably silver's rarity

---
+++ kubejs/server_scripts/Born in Chaos/smelting_recipes.js
@@ -0,0 +1,5 @@
+ServerEvents.recipes(event => {
+    event.recipes.minecraft.smelting('born_in_chaos_v1:rotten_fish', 'born_in_chaos_v1:smoked_fish')
+    event.campfireCooking('born_in_chaos_v1:rotten_fish', 'born_in_chaos_v1:smoked_fish', 1, 120)
+}
+)
---
+++ kubejs/server_scripts/from_the_fog/herobrine_config_command.js
@@ -0,0 +1,17 @@
+ServerEvents.customCommand('fog', event => {
+  event.server.runCommandSilent('tellraw @a "Hello"');
+  event.server.runCommandSilent('function watching:config/sighting_chance/3_rare');
+  event.server.runCommandSilent('function watching:config/start_delay/add');
+  event.server.runCommandSilent('function watching:config/start_delay/add');
+  event.server.runCommandSilent('function watching:config/start_delay/add');
+  event.server.runCommandSilent('function watching:config/start_delay/add');
+  event.server.runCommandSilent('function watching:config/start_delay/add');
+  event.server.runCommandSilent('function watching:config/start_delay/add');
+  event.server.runCommandSilent('function watching:config/start_delay/add');
+  event.server.runCommandSilent('function watching:config/spot_noise/false');
+  event.server.runCommandSilent('function watching:config/herobrine_skin/custom/-1_custom');
+  event.server.runCommandSilent('function watching:config/jumpscare/false');
+  event.server.runCommandSilent('function watching:config/creeping/false');
+  event.server.runCommandSilent('function watching:config/no_sleep/false');
+  event.server.runCommandSilent('function watching:config/nightmare/false');
+})
