eba39a873591fdc1d295930ade8db887ddeeb9e2
SpookySquid14 <hunter.c.donald@gmail.com>
2026-04-18 11:12:11 -0400

    Restricted stronger mob spawns to later in the game

---
+++ kubejs/server_scripts/Modpack/Tags/aquamirae_tag_removal.js
@@ -0,0 +1,3 @@
+ServerEvents.tags('worldgen/biome', event => {
+    event.remove('aquamirae:ice_maze','minecraft:frozen_ocean')
+})
---
+++ kubejs/server_scripts/from_the_fog/herobrine_config.js
@@ -0,0 +1,24 @@
+ServerEvents.loaded(event => {
+console.log(event.server.persistentData.loaded)
+if(!event.server.persistentData.loaded){
+  event.server.runCommand('tellraw @a "Hello"');
+  event.server.runCommand('function watching:config/sighting_chance/3_rare');
+  event.server.runCommand('function watching:config/start_delay/add');
+  event.server.runCommand('function watching:config/start_delay/add');
+  event.server.runCommand('function watching:config/start_delay/add');
+  event.server.runCommand('function watching:config/start_delay/add');
+  event.server.runCommand('function watching:config/start_delay/add');
+  event.server.runCommand('function watching:config/start_delay/add');
+  event.server.runCommand('function watching:config/start_delay/add');
+  event.server.runCommand('function watching:config/spot_noise/false');
+  event.server.runCommand('function watching:config/herobrine_skin/custom/-1_custom');
+  event.server.runCommand('function watching:config/jumpscare/false');
+  event.server.runCommand('function watching:config/creeping/false');
+  event.server.runCommand('function watching:config/no_sleep/false');
+  event.server.runCommand('function watching:config/nightmare/false');
+  //console.log(event.server.overworld().time, event.server.overworld().time < 24000)
+  if(event.server.overworld().time < 24000)
+    event.server.runCommand('time set day');
+  event.server.persistentData.loaded = true
+}
+})
