3312deb81300fcd2e5fdc4037368a454a96a072a
SpookySquid14 <hunter.c.donald@gmail.com>
2026-04-29 21:04:44 -0400

    Added Immersive Optimization

---
+++ kubejs/startup_scripts/cataclysm_spellcastng/cyber_spell_school.js
@@ -0,0 +1,28 @@
+//Creating attributes
+StartupEvents.registry('attribute', event => {
+	// Use the `spell` type for these attributes and set all the values needed. The default value has to be higher than the minimum value and lower than the maximum value.
+	event.create('cyber_spell_power', 'irons_spells_js:spell').setDefaultValue(1).setMinimumValue(0).setMaximumValue(10)
+	event.create('cyber_spell_resistance', 'irons_spells_js:spell').setDefaultValue(1).setMinimumValue(0).setMaximumValue(10)
+})
+//giving all mobs attributes
+EntityJSEvents.attributes(event => {
+    event.allTypes.forEach(type => {
+        event.modify(type, (a) => {
+            a.add('kubejs:cyber_spell_power')
+            a.add('kubejs:cyber_spell_resistance')
+        })
+    })
+})
+//registering school
+StartupEvents.registry('irons_spellbooks:schools', event => {
+	// You can use the ID of this school in any spell you create, or to change the school of an existing spell using the Iron's Spells server config.
+	event.create('cyber')
+		.setName(Component.of('Cyber').red()) // This sets what the school will be displayed as. This needs to be a Component.
+		.setFocus('cataclysm_spellcasting:cyber_focus') // The focus needs to be an item tag. You can add this same tag to an item to make it the focus.
+		.setPowerAttribute('kubejs:cyber_spell_power') // This sets the power attribute.
+		.setResistanceAttribute('kubejs:cyber_spell_resistance') // This sets the resistance attribute.
+		.setDefaultCastSound('cataclysm:harbinger_laser') // You can also set a default cast sound for each spell in the school.
+		// In 1.20.1 and up, creating spell schools requires a damage type to be inputted.
+		// You can create a damage type using datapacks. https://minecraft.wiki/w/Damage_type
+		.setDamageType('cataclysm_spellcasting:cyber_damage')
+})
