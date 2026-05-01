5d0c2da702cdf3d228277a33b23d59259789f834
SpookySquid14 <hunter.c.donald@gmail.com>
2026-05-01 10:13:11 -0400

    added axe tags to axes that lacked them.

---
+++ kubejs/server_scripts/Modpack/Tags/axes.js
@@ -0,0 +1,8 @@
+ServerEvents.tags("item", event => {
+
+    event.add("minecraft:axes", [
+        "cataclysm:black_steel_axe",
+        "werewolves:silver_axe",
+        "amethyst_tools:amethyst_axe"
+    ])
+})
