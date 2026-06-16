9685d01b60a57bf45239f55b3a392bd9a0e216e7
SpookySquid14 <hunter.c.donald@gmail.com>
2026-06-16 09:42:07 -0400

    - Tweaked Goety Quest Chapter - Added Saint's Dragons Quest Chapter - Gave dragon food tags to all Tide's fish - Disabled Forge update checker

--- kubejs/server_scripts/Modpack/Tags/foods.js
+++ kubejs/server_scripts/Modpack/Tags/foods.js
@@ -1,4 +1,10 @@
 ServerEvents.tags("item", event => {
 
-    event.add("forge:raw_fishes",'#tide:cookable_fish' )
+    event.add("forge:raw_fishes",'#tide:cookable_fish')
+
+    const dragon_food_tags = ['saintsdragons:dragon_food','saintsdragons:foods/varasuchus','saintsdragons:foods/cindervane','saintsdragons:foods/raevyx','saintsdragons:foods/ignivorus','saintsdragons:foods/stegonaut','saintsdragons:foods/volitans']
+
+    dragon_food_tags.forEach(food_tag => {
+        event.add(food_tag,"#tide:fish")
+    });    
 })
