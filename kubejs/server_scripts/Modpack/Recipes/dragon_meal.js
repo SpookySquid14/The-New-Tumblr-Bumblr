13970ed58eb4914cb870b21c9d2a966b2beab508
SpookySquid14 <hunter.c.donald@gmail.com>
2026-07-01 14:09:32 -0400

    - Made first quest unlock all others - Fixed recipe for Hearty Dragon Meal - Made player spawn in with quest book

--- kubejs/server_scripts/Modpack/Recipes/dragon_meal.js
+++ kubejs/server_scripts/Modpack/Recipes/dragon_meal.js
@@ -3,12 +3,12 @@ ServerEvents.recipes(event => {
     event.replaceInput(
         {output:'saintsdragons:hearty_dragon_meal'},
         'minecraft:cod',
-        'tumbrbumblr:dragon_meal_fish_med'
+        '#tumbrbumblr:dragon_meal_fish_med'
     )
     event.replaceInput(
         {output:'saintsdragons:hearty_dragon_meal'},
         'minecraft:salmon',
-        'tumbrbumblr:dragon_meal_fish_large'
+        '#tumbrbumblr:dragon_meal_fish_large'
     )
     event.replaceInput(
         {output:'saintsdragons:hearty_dragon_meal'},
