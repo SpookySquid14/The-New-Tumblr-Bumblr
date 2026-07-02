1e49820383c1fb99530c8313dd7721f70e3b787b
SpookySquid14 <hunter.c.donald@gmail.com>
2026-07-01 23:51:39 -0400

    - Remade Unstable Nether Portal gateway - Added buttons to go to shops from first quest chapter

--- kubejs/server_scripts/from_the_fog/herobrine_config.js
+++ kubejs/server_scripts/from_the_fog/herobrine_config.js
@@ -1,25 +1,43 @@
-ServerEvents.loaded(event => {
-console.log(event.server.persistentData.loaded)
-if(!event.server.persistentData.loaded){
-  event.server.runCommand('tellraw @a "Hello"');
-  event.server.runCommand('function watching:config/sighting_chance/3_rare');
-  event.server.runCommand('function watching:config/start_delay/add');
-  event.server.runCommand('function watching:config/start_delay/add');
-  event.server.runCommand('function watching:config/start_delay/add');
-  event.server.runCommand('function watching:config/start_delay/add');
-  event.server.runCommand('function watching:config/start_delay/add');
-  event.server.runCommand('function watching:config/start_delay/add');
-  event.server.runCommand('function watching:config/start_delay/add');
-  event.server.runCommand('function watching:config/spot_noise/false');
-  event.server.runCommand('function watching:config/herobrine_skin/custom/-1_custom');
-  event.server.runCommand('function watching:config/jumpscare/false');
-  event.server.runCommand('function watching:config/creeping/false');
-  event.server.runCommand('function watching:config/no_sleep/false');
-  event.server.runCommand('function watching:config/nightmare/false');
-  event.server.runCommand('function watching:config/glowing_eyes/false')
-  //console.log(event.server.overworld().time, event.server.overworld().time < 24000)
-  if(event.server.overworld().time < 24000)
-    event.server.runCommand('time set day');
-  event.server.persistentData.loaded = true
-}
+// ServerEvents.loaded(event => {
+// console.log(event.server.persistentData.loaded)
+// if(!event.server.persistentData.loaded){
+//   event.server.runCommand('tellraw @a "Hello"');
+//   event.server.runCommand('function watching:config/sighting_chance/3_rare');
+//   event.server.runCommand('function watching:config/start_delay/add');
+//   event.server.runCommand('function watching:config/start_delay/add');
+//   event.server.runCommand('function watching:config/start_delay/add');
+//   event.server.runCommand('function watching:config/start_delay/add');
+//   event.server.runCommand('function watching:config/start_delay/add');
+//   event.server.runCommand('function watching:config/start_delay/add');
+//   event.server.runCommand('function watching:config/start_delay/add');
+//   event.server.runCommand('function watching:config/spot_noise/false');
+//   event.server.runCommand('function watching:config/herobrine_skin/custom/-1_custom');
+//   event.server.runCommand('function watching:config/jumpscare/false');
+//   event.server.runCommand('function watching:config/creeping/false');
+//   event.server.runCommand('function watching:config/no_sleep/false');
+//   event.server.runCommand('function watching:config/nightmare/false');
+//   event.server.runCommand('function watching:config/glowing_eyes/false')
+//   //console.log(event.server.overworld().time, event.server.overworld().time < 24000)
+//   if(event.server.overworld().time < 24000)
+//     event.server.runCommand('time set day');
+//   event.server.persistentData.loaded = true
+// }
+// })
+
+ServerEvents.customCommand('fog', event => {
+  event.server.runCommandSilent('tellraw @a "Hello"');
+  event.server.runCommandSilent('function watching:config/sighting_chance/3_rare');
+  event.server.runCommandSilent('function watching:config/start_delay/add');
+  event.server.runCommandSilent('function watching:config/start_delay/add');
+  event.server.runCommandSilent('function watching:config/start_delay/add');
+  event.server.runCommandSilent('function watching:config/start_delay/add');
+  event.server.runCommandSilent('function watching:config/start_delay/add');
+  event.server.runCommandSilent('function watching:config/start_delay/add');
+  event.server.runCommandSilent('function watching:config/start_delay/add');
+  event.server.runCommandSilent('function watching:config/spot_noise/false');
+  event.server.runCommandSilent('function watching:config/herobrine_skin/custom/-1_custom');
+  event.server.runCommandSilent('function watching:config/jumpscare/false');
+  event.server.runCommandSilent('function watching:config/creeping/false');
+  event.server.runCommandSilent('function watching:config/no_sleep/false');
+  event.server.runCommandSilent('function watching:config/nightmare/false');
 })
