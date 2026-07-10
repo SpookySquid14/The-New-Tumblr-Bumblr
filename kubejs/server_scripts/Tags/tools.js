8aab2249e4c1467202f108d2f28a66231d52331c
SpookySquid14 <hunter.c.donald@gmail.com>
2026-07-10 15:18:55 -0400

    - Updated Ars Elemental - Updated Botania - Updated Create: Gunsmithing - Updated Collective - Updated Complicated Bees - Updated Creative Core - Updated Ember's Castles and Keeps - Updated Ember's Floating Islands - Updated Ember's Underground Rooms - Updated Entity Culling - Updated ExtraLib - Updated ForgeEnderTech - Updated Fusion - Updated Geckolib - Updated Goety - Updated Guard Villagers - Updated GuideMe - Updated Illagers Wear Armor - Updated ImmediatelyFast - Updated Immersive Aircraft - Updated Immersive Optimization - Updated Iron's Lib - Updated Iron's Spells and Spellbooks - Updated L_Ender's Cataclysm - Updated Lightspeed - Modernfix - Updated More Relics - Music Events - Updated Particular Reforged - Updated Platform - Updated Pufferfish's Skills - Updated Selene - Updated Sophisticated Backpacks - Updated Sophisticated Core - Updated Sophisticated Storage - Updated Spells Gone Wrong - Updated Structurify - Updated Stylish Effects - Updated Tide 2 - Updated Vampirism - Updated Vanilla Backport - Added Spears Plus - Readjusted Fishing quest chapter - Added tags to copper items

--- kubejs/server_scripts/Tags/tools.js
+++ kubejs/server_scripts/Tags/tools.js
@@ -10,4 +10,22 @@ ServerEvents.tags("item", event => {
     event.add("forge:tools/pickaxes", [
         "tumblrbumblr:copper_pickaxe"
     ])
+    event.add("forge:tools/shovel", [
+        "tumblrbumblr:copper_shovel"
+    ])
+    event.add("forge:tools/hoe", [
+        "tumblrbumblr:copper_hoe"
+    ])
+    event.add("forge:chestplates", [
+        "tumblrbumblr:copper_chestplate"
+    ])
+    event.add("forge:helmets", [
+        "tumblrbumblr:copper_helmet"
+    ])
+    event.add("forge:leggings", [
+        "tumblrbumblr:copper_leggings"
+    ])
+    event.add("forge:boots", [
+        "tumblrbumblr:copper_boots"
+    ])
 })
