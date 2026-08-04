3e6e37310ee24b70a2794c0fe2cb5b478aedbdab
SpookySquid14 <hunter.c.donald@gmail.com>
2026-08-04 12:00:24 -0400

    - Enabled Corail Tombstone features - Added bones to 'c:bones' tag

--- kubejs/server_scripts/Tags/items.js
+++ kubejs/server_scripts/Tags/items.js
@@ -2,5 +2,6 @@ ServerEvents.tags("item", event => {
 
     event.add("cataclysm:explosion_immune_item",'endrem:wither_eye')
     event.add("cataclysm:explosion_immune_item",'tumblrbumblr:nether_star_drained')
+    event.add("c:bones", 'minecraft:bone')
  
 })
