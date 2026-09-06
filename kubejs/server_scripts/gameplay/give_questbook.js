7565852289969689f3614cf70057742bb30bb61c
SpookySquid14 <hunter.c.donald@gmail.com>
2026-09-05 23:20:18 -0400

    - Installed Crawl On Demand - Installed Astikor Carts Redux - Installed Mekanism Tools - Installed Vampires Need Umbrellas - Removed script that gives player FTB questbook

--- kubejs/server_scripts/gameplay/give_questbook.js
+++
@@ -1,8 +0,0 @@
-PlayerEvents.loggedIn(event => {
-	const pData = event.player.persistentData
-	if (!pData.firstJoin) {
-		pData.firstJoin = true
-		// Do stuff only on first join, here we give the player some diamonds and a sword
-		event.player.give('ftbquests:book')
-	}
-})
