0b6b02c7f16c027150de1151243f5e1544a67653
SpookySquid14 <hunter.c.donald@gmail.com>
2026-09-14 21:36:26 -0400

    - Added Fishing Questline - Tweaked Generation config for Cataclysm Dimensions

---
+++ kubejs/server_scripts/tags/bosses.js
@@ -0,0 +1,5 @@
+ServerEvents.tags('entity_type', event => {
+    event.add('c:bosses','fdbosses:chesed')
+    event.add('c:bosses','fdbosses:malkuth')
+    event.add('c:bosses','fdbosses:geburah')
+})
